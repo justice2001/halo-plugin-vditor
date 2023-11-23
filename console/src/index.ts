973077453e2e7b1cb5b63a57861ac27fad1f6c5e
zhengyi <admin@mczhengyi.top>
2023-11-23 13:09:46 +0800

    feat: run vditor on halo

--- console/src/index.ts
+++ console/src/index.ts
@@ -1,29 +1,22 @@
-import { definePlugin } from "@halo-dev/console-shared";
+import {definePlugin, type EditorProvider} from "@halo-dev/console-shared";
 import HomeView from "./views/HomeView.vue";
-import { IconPlug } from "@halo-dev/components";
+import logo from "./assets/vditor.png"
 import { markRaw } from "vue";
 
 export default definePlugin({
   components: {},
-  routes: [
-    {
-      parentName: "Root",
-      route: {
-        path: "/example",
-        name: "Example",
-        component: HomeView,
-        meta: {
-          title: "示例页面",
-          searchable: true,
-          menu: {
-            name: "示例页面",
-            group: "示例分组",
-            icon: markRaw(IconPlug),
-            priority: 0,
-          },
+  extensionPoints: {
+    "editor:create": (): EditorProvider[] => {
+      return [
+        {
+          name: "Vditor",
+          displayName: "Vditor Markdown",
+          // @ts-ignore
+          component: HomeView,
+          rawType: "markdown",
+          logo: logo
         },
-      },
+      ];
     },
-  ],
-  extensionPoints: {},
+  },
 });
