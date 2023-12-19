2cfce64d3176200fcb3c414b02ed4cac875838c6
zhengyi <admin@mczhengyi.top>
2023-12-19 11:40:36 +0800

    :arrow_up: Update halo-vditor to 3.9.8

--- download_dist.sh
+++ download_dist.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 cd src/main/resources/static
+rm -rf dist
 pwd
 curl -o vditor.tgz \
- https://git.mczhengyi.top/zhengyi/-/packages/npm/@zhengyi%2Fvditor/3.9.7/files/248
+ https://git.mczhengyi.top/zhengyi/-/packages/npm/@zhengyi%2Fvditor/3.9.8/files/249
 tar -xzvf vditor.tgz
 mv package/dist .
 rm -rf package
