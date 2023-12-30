b6b9017e57f24d6cd6850cc4df30c142ebb930a8
zhengyi <admin@mczhengyi.top>
2023-12-30 15:16:40 +0800

    :arrow_up: Update Halo-Vditor to 3.9.9
    Fix https://git.mczhengyi.top/zhengyi/halo-plugin-vditor/issues/32
    Fix https://git.mczhengyi.top/zhengyi/halo-plugin-vditor/issues/25

--- download_dist.sh
+++ download_dist.sh
@@ -4,7 +4,7 @@ cd src/main/resources/static
 rm -rf dist
 pwd
 curl -o vditor.tgz \
- https://git.mczhengyi.top/zhengyi/-/packages/npm/@zhengyi%2Fvditor/3.9.9-build.1/files/257
+ https://git.mczhengyi.top/zhengyi/-/packages/npm/@zhengyi%2Fvditor/3.9.9/files/258
 tar -xzvf vditor.tgz
 mv package/dist .
 rm -rf package
