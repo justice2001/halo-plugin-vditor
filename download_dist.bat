2ea456db53136ba4ddd14393b426f53b28e64c3b
zhengyi <admin@mczhengyi.top>
2024-01-10 15:16:35 +0800

    :sparkles: Add Gallery
    https://github.com/justice2001/halo-plugin-vditor/issues/12
    https://git.mczhengyi.top/zhengyi/halo-plugin-vditor/issues/37

--- download_dist.bat
+++ download_dist.bat
@@ -1,7 +1,7 @@
 cd src/main/resources/static
 if exist dist rmdir /s /q dist
 echo %cd%
-bitsadmin /transfer vditorDownloadJob https://git.mczhengyi.top/zhengyi/-/packages/npm/@zhengyi%%2Fvditor/3.9.9/files/258 %cd%\vditor.tgz
+bitsadmin /transfer vditorDownloadJob https://git.mczhengyi.top/zhengyi/-/packages/npm/@zhengyi%%2Fvditor/3.9.10-beta.3/files/261 %cd%\vditor.tgz
 7z x vditor.tgz
 7z x vditor.tar
 move /y %cd%\package\dist %cd%
