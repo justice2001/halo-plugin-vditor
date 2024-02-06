4eea179a248c50a89e41fff0ddd96e47aaab0517
zhengyi <admin@mczhengyi.top>
2024-02-06 23:09:42 +0800

    🐛  Compatible old version render

--- src/main/resources/static/external/halo-renders.js
+++ src/main/resources/static/external/halo-renders.js
@@ -1,10 +1,26 @@
 vditorRender.addExternal((conf) => {
     document.querySelectorAll(".language-halo").forEach(el => {
-        console.log(el.parentElement)
         // TODO: Vditor未对getHTML的代码进行处理
-        el.parentElement.outerHTML = HaloJs.renderHalo(
+        // 兼容老版本
+        let oldVersion = false;
+        const children = el.parentElement.children
+        for (let i = 0; i < children.length.length; i++) {
+            const child = children[i];
+            if (child.getAttribute("id") === "vditor-article-sign") {
+                oldVersion = true
+                break
+            }
+        }
+        if (oldVersion) {
+            el.outerHTML = HaloJs.renderHalo(
                 el.textContent,
                 "/plugins/vditor-mde/assets/static/halo-renders"
-        );
+            );
+        } else {
+            el.parentElement.outerHTML = HaloJs.renderHalo(
+                el.textContent,
+                "/plugins/vditor-mde/assets/static/halo-renders"
+            );
+        }
     })
 })
