7581ccb6fc347a6d04b367fe8d848cb48a98ac89
zhengyi <admin@mczhengyi.top>
2024-03-23 14:05:05 +0800

    ✨Add cdn settings for post render
    Fix https://git.mczhengyi.top/zhengyi/halo-plugin-vditor/issues/57
    Fix https://github.com/justice2001/halo-plugin-vditor/issues/30

--- src/main/java/top/mczhengyi/vditor/bean/RenderConfig.java
+++ src/main/java/top/mczhengyi/vditor/bean/RenderConfig.java
@@ -4,8 +4,19 @@ import lombok.Data;
 
 @Data
 public class RenderConfig {
+    private final String VDITOR_VERSION = "3.9.9";
+
     Boolean enableRender;
     String darkMode;
     Boolean mediaRender;
     Boolean onlyMarkdown;
+    String cdn;
+    String cdnUrl;
+
+    public String getCdn() {
+        if ("self-hosted".equals(cdn)) {
+            return cdnUrl.replace("${v}", VDITOR_VERSION);
+        }
+        return cdn.replace("${v}", VDITOR_VERSION);
+    }
 }
