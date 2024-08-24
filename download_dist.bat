144bc23e7e389ba1da2d8318bbb9038366975e9a
zhengyi59 <admin@mczhengyi.top>
2024-08-24 16:18:59 +0800

    ⬆️Update vditor version to 3.10.4

--- download_dist.bat
+++ download_dist.bat
@@ -1,7 +1,7 @@
 cd src/main/resources/static
 if exist dist rmdir /s /q dist
 echo %cd%
-bitsadmin /transfer vditorDownloadJob https://registry.npmjs.org/vditor/-/vditor-3.10.2.tgz %cd%\vditor.tgz
+bitsadmin /transfer vditorDownloadJob https://registry.npmjs.org/vditor/-/vditor-3.10.4.tgz %cd%\vditor.tgz
 7z x vditor.tgz
 7z x vditor.tar
 move /y %cd%\package\dist %cd%
