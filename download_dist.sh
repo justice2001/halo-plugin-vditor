a579875622eb5223c77aa9aa4e94de14977f174c
zhengyi59 <admin@mczhengyi.top>
2025-05-10 16:52:31 +0800

    ⬆️  Update vditor to 3.11.0

--- download_dist.sh
+++ download_dist.sh
@@ -4,7 +4,7 @@ cd src/main/resources/static
 rm -rf dist
 pwd
 curl -o vditor.tgz \
- https://registry.npmjs.org/vditor/-/vditor-3.10.4.tgz
+ https://registry.npmjs.org/vditor/-/vditor-3.11.0.tgz
 tar -xzvf vditor.tgz
 mv package/dist .
 rm -rf package
